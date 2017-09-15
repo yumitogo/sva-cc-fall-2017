f309315feb41dc2a32adb1116194fb110e919e7a
Jin Sung Yoo <jyoo6@sva.com>
2017-09-15 13:48:43 -0400

    add an example for week 2

---
+++ week02/examples/setup_draw/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  //background(200);
+  fill(50);
+  ellipse(width/2, height/2, 50, 50);
+  fill(150)
+  ellipse(mouseX, mouseY, 50, 50);
+}
