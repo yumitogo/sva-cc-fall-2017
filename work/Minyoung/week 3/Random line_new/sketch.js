7094735b525e9fd740c15ab0e599e746240beaf2
mpark31 <32002309+mpark31@users.noreply.github.com>
2017-09-22 21:25:39 -0400

    week 3 work_1

--- work/Minyoung/week 3/Random line_new/sketch.js
+++ work/Minyoung/week 3/Random line_new/sketch.js
@@ -10,9 +10,9 @@ function draw() {
   var yPos = random ( 20 ) -10;
   var randomSize = 5 +random(50);
   myhue += 1;
-  stroke(255, 50);
-  fill(myhue, 30, 50);
-  strokeWeight(1);
+  stroke(myhue, 30, 255);
+  //fill(myhue, 30, 255);
+  strokeWeight(5);
 
   if (myhue > 300) {
     myhue = 0;
@@ -21,9 +21,10 @@ function draw() {
   Hi! I'm Minyoung! */
   //line(mouseX, mouseY, pmouseX, pmouseY);
 
+  //var myName = "Minyoung";
   if( mouseIsPressed) {
-    //line(mouseX, mouseY, pmouseX+xPos, pmouseY+yPos);
-    ellipse(mouseX+xPos, mouseY+yPos, randomSize, randomSize);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    //ellipse(mouseX+xPos, mouseY+yPos, randomSize, randomSize);
   }
 
   if(keyIsPressed) {
