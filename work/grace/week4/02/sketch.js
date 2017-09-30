2ab28f45869ca8f9caea9d37751a82493bcc5ad3
gdai573 <32002314+gdai573@users.noreply.github.com>
2017-09-29 21:18:57 -0400

    grace week4

--- work/grace/week4/02/sketch.js
+++ work/grace/week4/02/sketch.js
@@ -6,29 +6,23 @@ function setup() {
 var xPos = 200;
 var yPos = 200;
 
-
 function draw() {
   background(200);
 
-  var redColor = frameCount;
-  var sizeOfCircle = 100;
+  var redColor = 255;
 
   fill(redColor, 0, 0);
-  ellipse(mouseX, mouseY, sizeOfCircle, sizeOfCircle);
 
-  redColor -= 20;
-  sizeOfCircle = sizeOfCircle - 20;
-  fill(redColor, 0, 0);
-  ellipse(xPos, yPos, sizeOfCircle, sizeOfCircle);
+  //for ( var sizeOfCircle = 100; sizeOfCircle > 0; sizeOfCircle = sizeOfCircle - 10) {
+  var myVar = 10;
 
-  redColor -= 20;
-  sizeOfCircle = sizeOfCircle - 20;
-  fill(redColor, 0, 0);
-  ellipse(xPos, yPos, sizeOfCircle, sizeOfCircle);
 
-  redColor -= 20;
-  sizeOfCircle = sizeOfCircle - 20;
-  fill(redColor, 0, 0);
-  ellipse(xPos, yPos, sizeOfCircle, sizeOfCircle);
+  for (var i = 0; i < 30; i++) {
 
+    Math.sin(Math.PI);
+    redColor -= 2;
+    fill(redColor, 0, 0);
+    var circleSize = width - i*3;
+    ellipse(xPos, yPos, circleSize, circleSize);
+  }
 }
