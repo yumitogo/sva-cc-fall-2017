21b450a307980181bdff0531d979fab433ecbdc3
Jin Sung Yoo <ujinsung@gmail.com>
2017-10-18 21:09:20 -0400

    add examples for week7

--- week07/examples/06_class_in_file/arrow.js
+++ week07/examples/06_class_in_file/arrow.js
@@ -30,18 +30,3 @@ function Arrow (x, y, size) {
     return angleInRad;
   }
 }
-
-var arrow1;
-var arrow2;
-
-function setup() {
-  createCanvas(500, 500);
-  arrow1 = new Arrow(200, 100, 100);
-  arrow2 = new Arrow(200, 200, 100);
-}
-
-function draw() {
-  background(200);
-  arrow1.draw();
-  arrow2.draw();
-}
