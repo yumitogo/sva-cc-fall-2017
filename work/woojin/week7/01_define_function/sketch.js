821337a33972e2e065c7c89c6db0b4cefbdbe29f
woojin00 <31783607+woojin00@users.noreply.github.com>
2017-10-20 21:30:04 -0400

    week7

---
+++ work/woojin/week7/01_define_function/sketch.js
@@ -0,0 +1,18 @@
+var myArrows = [];
+
+function setup() {
+  createCanvas(500, 500);
+
+  for (var i = 0; i < 100; i++) {
+    myArrows[i] = new Arrow( 5 * i, 100);
+  }
+}
+
+function draw() {
+  background(200);
+  fill(0);
+  stroke(0);
+  for(var i = 0; i < myArrows.length; i++) {
+    myArrows[i].draw();
+  }
+}
