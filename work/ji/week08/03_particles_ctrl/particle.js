fcbe0467d0a602197f7d53c068690311ad17e3f7
jisoo966 <32002313+jisoo966@users.noreply.github.com>
2017-10-27 21:18:12 -0400

    week 08

---
+++ work/ji/week08/03_particles_ctrl/particle.js
@@ -0,0 +1,18 @@
+function Particles(x, y, size) {
+  this.x = x;
+  this.y = y;
+  this.size = size;
+
+  this.update = function (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  this.draw = function (index) {
+    push();
+    translate (this.x, this.y);
+    rotate(Math.PI / 100 * index);
+    rect(0, 0, this.size, this.size);
+    pop();
+  }
+}
