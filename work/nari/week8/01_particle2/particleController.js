b80bbd4c77a667f9a6daf8b15fb9ede6ef42ecab
narinari8 <31897034+narinari8@users.noreply.github.com>
2017-10-27 21:18:27 -0400

    week8

---
+++ work/nari/week8/01_particle2/particleController.js
@@ -0,0 +1,28 @@
+function particleController() {
+   this.particles = [];
+   this.numOfParticles;
+   this.currentParticleIndex = 0;
+
+   this.setup = function(numOfParticles) {
+     for (var i = 0; i < numOfParticles; i++) {
+       this.particles[i] = new Particle(-100, -100, 100);
+     }
+   };
+
+   this.update = function (x, y) {
+     this.currentParticleIndex++;
+     this.currentParticleIndex = this.currentParticleIndex % this.particles.length;
+     this.particles[this.currentParticleIndex].update(x, y);
+   };
+
+   this.draw = function() {
+     var numOfParticles = this.particles.length;
+     for (var i = 0; i < this.particles.length; i++) {
+       fill(i/numOfParticles * 255, 150, 250);
+
+       //modulo
+       var index = (this.currentParticleIndex + i) % numOfParticles;
+       this.particles[index].draw(index);
+     }
+   }
+}
