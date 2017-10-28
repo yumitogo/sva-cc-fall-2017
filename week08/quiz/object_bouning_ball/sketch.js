490ab7c21cf98d8e8caf887568749cda25d72943
jinsungsva <31589495+jinsungsva@users.noreply.github.com>
2017-10-27 21:17:58 -0400

    week 8 work

--- week08/quiz/object_bouning_ball/sketch.js
+++ week08/quiz/object_bouning_ball/sketch.js
@@ -1,25 +1,32 @@
-var ball1;
+var balls= [];
 
 function setup() {
   createCanvas(400, 400);
-  // create a Ball class to make Ball object by calling new command.
-  // ball1 = new Ball();
-  ball1 = {
-    x:width/2,
-    y:height,
-    size: 10,
-    move: function() {
-      this.x = this.x + Math.random() - 0.5;
-      this.y = this.y - Math.random()*0.5;
-    },
-    display: function() {
-      ellipse(this.x, this.y, this.size, this.size);
-    }
+  rectMode(CENTER);
+  for (var i=0; i<3; i++){
+    balls[i] = new Ball(100+100 * i, height);
   }
+
 }
 
 function draw() {
   background(100);
-  ball1.move();
-  ball1.display();
+  for (var i = 0; i<balls.length; i++) {
+    balls[i].move();
+    balls[i].display();
+  }
+
+}
+
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.size = 10;
+  this.move = function () {
+    this.x = this.x + Math.random() - 0.5;
+    this.y = this.y - Math.random() * 0.5;
+  };
+  this.display = function () {
+    ellipse(this.x, this.y, this.size, this.size);
+  }
 }
---
+++ work/jinsung/week8/01_particle/sketch.js
@@ -0,0 +1,19 @@
+var numOfParticles = 100;
+var pc;
+
+function setup() {
+  createCanvas(400, 400);
+  colorMode (HSB, 255);
+  rectMode(CENTER);
+  noStroke();
+
+  pc = new ParticleController();
+  pc.setup(numOfParticles);
+
+}
+
+function draw() {
+  background(100);
+  pc.update(mouseX, mouseY);
+  pc.draw();
+}
